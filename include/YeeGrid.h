e61ff3d608b60e2ba04b4f4a1a3468a3719bdfdb
Nima Ch <nimachamanara@gmail.com>
2018-09-12 21:07:25 -0400

    designing the YeeGrid class..

--- include/YeeGrid.h
+++ include/YeeGrid.h
@@ -2,8 +2,29 @@
 #ifndef FDTD_YEEGRID_H_
 #define FDTD_YEEGRID_H_
 
-// extends Grid and defines a Yee FDTD grid, where electric and magnetic nodes are staggered in space and time.
-//
+
+#include "NumberTypes.h"
+
+class YeeGrid {
+    
+    YeeGrid(int nDim);
+    
+    int nDim;
+    std::vector<std::size_t> nPoints;
+    
+    std::vector<YeeGridScalar&> scalars;
+
+    std::vector<YeeGridEScalarArray&> eScalarArrays; 
+    std::vector<YeeGridHScalarArray&> hScalarArrays; 
+
+    std::vector<YeeGridEVectorArray&> eVectorArrays; 
+    std::vector<YeeGridHVectorArray&> hVectorArrays; 
+    
+    std::vector<YeeGridEMatrixArray&> eMatrixArrays; 
+    std::vector<YeeGridHMatrixArray&> hMatrixArrays; 
+    
+}
+
 
 
 
