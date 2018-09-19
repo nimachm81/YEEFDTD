c737f57067ef2d1f2255ee802d60a0c662b7d2bc
Nima Ch <nimachamanara@gmail.com>
2018-09-19 17:28:36 -0400

    added vector3..

--- include/YeeGrid.h
+++ include/YeeGrid.h
@@ -12,12 +12,21 @@
 #include "YeeGridDataTypes.h"
 
 
+enum class FDInstructionCode {
+    A_pe_b_Cijk   // A[i][j][k] += b*(C[i+i0][j+j0][k+k0] + C[i+i1][j+j1][k+k1] + ... ) fir ijk in the range
+                  // ind_start...ind_end
+                  // parameters : tuple{pair(ind_start, ind_end), b, C, vector<[i0,j0,k0]>} with types
+                  // std::tuple<std::pair<std::array<int, 3>>, RealNumber, std::string, std::vector<std::array<RealNumber, 3>>>
+                  //
+};
 
 class YeeGrid3D {
     public:
     YeeGrid3D(std::array<std::size_t, 3>& nCells);
     void AddGridElement(const std::string name, ElementType elemType);
     YeeGridData3D& GetGridElement(const std::string name);
+
+    //void AddUpdateInstruction(instructionCode, parameters);
 
 
     private:
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
  //
 
-#include <any>
-#include <typeinfo>
+
 #include <iostream>
 
 #include "NumberTypes.h"
 #include "MultiDimArray.hpp"
 
-int main(int argc, char** argv) {
+
+#include <any>
+#include <typeinfo>
+void test_multidim_array() {
     std::cout << "Test." << std::endl;
 
     std::array<std::size_t, 3> shape{2,3,4};
@@ -43,6 +45,33 @@ int main(int argc, char** argv) {
 
     std::cout << " a : " << typeid(NumberArray3D<RealNumber>).name() << std::endl;
     std::any_cast<NumberArray3D<RealNumber>>(a).Print();
+}
+
+#include <tuple>
+void test_void_pointer() {
+    std::tuple<int, double> p1(1, 2.0);
+    std::cout << std::get<0>(p1) << "  " << std::get<1>(p1) << std::endl;
+
+    void* vp_p1 = &p1;
+    std::tuple<int, double>* p2 = static_cast<std::tuple<int, double>*>(vp_p1);
+    std::cout << std::get<0>(*p2) << "  " << std::get<1>(*p2) << std::endl;
+}
+
+#include "Vector.hpp"
+void test_vector3() {
+    Vector3<double> v1(1.0, 2.0, 3.0);
+    std::cout << "v1: " << v1 << std::endl;
+    Vector3<double> v2(4.0, 5.0, 7.5);
+    std::cout << "v2: " << v2 << std::endl;
+
+    Vector3<double> v = v1 + v2;
+    std::cout << "v1 + v2: " << v << std::endl;
+}
+
+
+int main(int argc, char** argv) {
+    test_vector3();
+
 }
 
 
