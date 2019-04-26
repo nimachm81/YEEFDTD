0028455fe5778c9ffee350794c2c35cc8dd93d64
Nima Ch <nimachamanara@gmail.com>
2019-04-26 08:27:56 -0400

    3D nonuniform grid top layer tested

--- test/Test_FDTD_3D_Nonuniform_Gridcollection_Json.hpp
+++ test/Test_FDTD_3D_Nonuniform_Gridcollection_Json.hpp
@@ -2,15 +2,24 @@
 
 
 void test_run_fdtd_3d_nonuniform_gridcollection_from_json() {
-    FPNumber x0 = 0;
-    FPNumber x1 = 2.0;
-    FPNumber y0 = 0;
-    FPNumber y1 = 2.0;
-    FPNumber z0 = 0.0;
-    FPNumber z1 = 2.0;
-    std::size_t nx = 100;
-    std::size_t ny = 100;
-    std::size_t nz = 100;
+
+    std::array<FPNumber, 3> box_in_r0 = {-1.0, -1.0, -1.0};
+    std::array<FPNumber, 3> box_in_r1 = {+1.0, +1.0, +1.0};
+
+    std::array<FPNumber, 3> box_out_r0 = {-3.0, -3.0, -3.0};
+    std::array<FPNumber, 3> box_out_r1 = {+3.0, +3.0, +3.0};
+
+    std::size_t n_xyz_in = 100;
+
+    FPNumber x0 = box_in_r0[0];
+    FPNumber x1 = box_in_r1[0];
+    FPNumber y0 = box_in_r0[1];
+    FPNumber y1 = box_in_r1[1];
+    FPNumber z0 = box_in_r0[2];
+    FPNumber z1 = box_in_r1[2];
+    std::size_t nx = n_xyz_in;
+    std::size_t ny = n_xyz_in;
+    std::size_t nz = n_xyz_in;
     FPNumber dx = (x1 - x0)/(FPNumber)(nx);
     FPNumber dy = (y1 - y0)/(FPNumber)(ny);
     FPNumber dz = (z1 - z0)/(FPNumber)(nz);
@@ -27,40 +36,55 @@ void test_run_fdtd_3d_nonuniform_gridcollection_from_json() {
         indxJ += 1;
     }
 
-    FPNumber gr_x0 = 0;
-    FPNumber gr_x1 = 2.0;
-    FPNumber gr_y0 = 0;
-    FPNumber gr_y1 = 2.0;
-    FPNumber gr_z0 = z1;
-    FPNumber gr_z1 = z1 + 2.0;
-    std::size_t gr_nx = 50;
-    std::size_t gr_ny = 50;
-    std::size_t gr_nz = 50;
+    FPNumber gr_x0 = box_in_r0[0];
+    FPNumber gr_x1 = box_in_r1[0];
+    FPNumber gr_y0 = box_in_r0[1];
+    FPNumber gr_y1 = box_in_r1[1];
+    FPNumber gr_z0 = box_in_r1[2];
+    FPNumber gr_z1 = box_out_r1[2];
+    std::size_t gr_nx = n_xyz_in/2;
+    std::size_t gr_ny = n_xyz_in/2;
+    std::size_t gr_nz = n_xyz_in/2;
     FPNumber gr_dx = (gr_x1 - gr_x0)/(FPNumber)(gr_nx);
     FPNumber gr_dy = (gr_y1 - gr_y0)/(FPNumber)(gr_ny);
     FPNumber gr_dz = (gr_z1 - gr_z0)/(FPNumber)(gr_nz);
     FPNumber gr_dt = 2.0*dt;
 
-    FPNumber gl_x0 = 0;
-    FPNumber gl_x1 = 2.0;
-    FPNumber gl_y0 = 0;
-    FPNumber gl_y1 = 2.0;
-    FPNumber gl_z0 = z0 - 2.0;
-    FPNumber gl_z1 = z0;
-    std::size_t gl_nx = 50;
-    std::size_t gl_ny = 50;
-    std::size_t gl_nz = 50;
+    FPNumber gl_x0 = box_in_r0[0];
+    FPNumber gl_x1 = box_in_r1[0];
+    FPNumber gl_y0 = box_in_r0[1];
+    FPNumber gl_y1 = box_in_r1[1];
+    FPNumber gl_z0 = box_out_r0[2];
+    FPNumber gl_z1 = box_in_r0[2];
+    std::size_t gl_nx = n_xyz_in/2;
+    std::size_t gl_ny = n_xyz_in/2;
+    std::size_t gl_nz = n_xyz_in/2;
     FPNumber gl_dx = (gl_x1 - gl_x0)/(FPNumber)(gl_nx);
     FPNumber gl_dy = (gl_y1 - gl_y0)/(FPNumber)(gl_ny);
     FPNumber gl_dz = (gl_z1 - gl_z0)/(FPNumber)(gl_nz);
     FPNumber gl_dt = 2.0*dt;
 
+    FPNumber gu_x0 = box_in_r0[0];
+    FPNumber gu_x1 = box_in_r1[0];
+    FPNumber gu_y0 = box_in_r1[1];
+    FPNumber gu_y1 = box_out_r1[1];
+    FPNumber gu_z0 = box_out_r0[2];
+    FPNumber gu_z1 = box_out_r1[2];
+    std::size_t gu_nx = n_xyz_in/2;
+    std::size_t gu_ny = n_xyz_in/2;
+    std::size_t gu_nz = n_xyz_in/2*3;
+    FPNumber gu_dx = (gu_x1 - gu_x0)/(FPNumber)(gu_nx);
+    FPNumber gu_dy = (gu_y1 - gu_y0)/(FPNumber)(gu_ny);
+    FPNumber gu_dz = (gu_z1 - gu_z0)/(FPNumber)(gu_nz);
+    FPNumber gu_dt = 2.0*dt;
 
     std::size_t gr_indxSave = indxJ / 2;
     std::size_t gl_indxSave = gr_indxSave;
+    std::size_t gu_indxSave = gr_indxSave;
 
     std::size_t gr_save_rate = 1;
     std::size_t gl_save_rate = gr_save_rate;
+    std::size_t gu_save_rate = gr_save_rate;
     std::size_t save_rate = 2*gr_save_rate;
 
     std::size_t numOfTimeSamples = 150;
@@ -168,6 +192,8 @@ void test_run_fdtd_3d_nonuniform_gridcollection_from_json() {
             {"\"_gl_nx_m1_\"", boost::lexical_cast<std::string>(gl_nx - 1)},
             {"\"_gl_ny_m1_\"", boost::lexical_cast<std::string>(gl_ny - 1)},
             {"\"_gl_nz_m1_\"", boost::lexical_cast<std::string>(gl_nz - 1)},
+            {"\"_gl_nz_p_nz2_\"", boost::lexical_cast<std::string>(gl_nz + nz/2)},
+            {"\"_gl_nz_p_nz2_m1_\"", boost::lexical_cast<std::string>(gl_nz + nz/2 - 1)},
             {"\"_gl_dx_\"", boost::lexical_cast<std::string>(std::real(gl_dx))},
             {"\"_gl_dy_\"", boost::lexical_cast<std::string>(std::real(gl_dy))},
             {"\"_gl_dz_\"", boost::lexical_cast<std::string>(std::real(gl_dz))},
@@ -187,6 +213,40 @@ void test_run_fdtd_3d_nonuniform_gridcollection_from_json() {
             {"\"_gl_indxSave_\"", boost::lexical_cast<std::string>(gl_indxSave)},
             {"\"_gl_indxSave_p1_\"", boost::lexical_cast<std::string>(gl_indxSave + 1)},
             {"\"_gl_save_rate_\"", boost::lexical_cast<std::string>(gl_save_rate)},
+            {"\"_gu_x0_\"", boost::lexical_cast<std::string>(std::real(gu_x0))},
+            {"\"_gu_x1_\"", boost::lexical_cast<std::string>(std::real(gu_x1))},
+            {"\"_gu_y0_\"", boost::lexical_cast<std::string>(std::real(gu_y0))},
+            {"\"_gu_y1_\"", boost::lexical_cast<std::string>(std::real(gu_y1))},
+            {"\"_gu_z0_\"", boost::lexical_cast<std::string>(std::real(gu_z0))},
+            {"\"_gu_z1_\"", boost::lexical_cast<std::string>(std::real(gu_z1))},
+            {"\"_gu_nx_\"", boost::lexical_cast<std::string>(gu_nx)},
+            {"\"_gu_ny_\"", boost::lexical_cast<std::string>(gu_ny)},
+            {"\"_gu_nz_\"", boost::lexical_cast<std::string>(gu_nz)},
+            {"\"_gu_nx_p1_\"", boost::lexical_cast<std::string>(gu_nx + 1)},
+            {"\"_gu_ny_p1_\"", boost::lexical_cast<std::string>(gu_ny + 1)},
+            {"\"_gu_nz_p1_\"", boost::lexical_cast<std::string>(gu_nz + 1)},
+            {"\"_gu_nx_m1_\"", boost::lexical_cast<std::string>(gu_nx - 1)},
+            {"\"_gu_ny_m1_\"", boost::lexical_cast<std::string>(gu_ny - 1)},
+            {"\"_gu_nz_m1_\"", boost::lexical_cast<std::string>(gu_nz - 1)},
+            {"\"_gu_dx_\"", boost::lexical_cast<std::string>(std::real(gu_dx))},
+            {"\"_gu_dy_\"", boost::lexical_cast<std::string>(std::real(gu_dy))},
+            {"\"_gu_dz_\"", boost::lexical_cast<std::string>(std::real(gu_dz))},
+            {"\"_gu_dt_\"", boost::lexical_cast<std::string>(std::real(gu_dt))},
+            {"\"_gu_dt_dx_\"", boost::lexical_cast<std::string>(std::real(gu_dt/gu_dx))},
+            {"\"_gu_m_dt_dx_\"", boost::lexical_cast<std::string>(std::real(-gu_dt/gu_dx))},
+            {"\"_gu_dt_dy_\"", boost::lexical_cast<std::string>(std::real(gu_dt/gu_dy))},
+            {"\"_gu_m_dt_dy_\"", boost::lexical_cast<std::string>(std::real(-gu_dt/gu_dy))},
+            {"\"_gu_dt_dz_\"", boost::lexical_cast<std::string>(std::real(gu_dt/gu_dz))},
+            {"\"_gu_m_dt_dz_\"", boost::lexical_cast<std::string>(std::real(-gu_dt/gu_dz))},
+            {"\"_gu_dt_dx_2_\"", boost::lexical_cast<std::string>(std::real(gu_dt/gu_dx/2.0))},
+            {"\"_gu_m_dt_dx_2_\"", boost::lexical_cast<std::string>(std::real(-gu_dt/gu_dx/2.0))},
+            {"\"_gu_dt_dy_2_\"", boost::lexical_cast<std::string>(std::real(gu_dt/gu_dy/2.0))},
+            {"\"_gu_m_dt_dy_2_\"", boost::lexical_cast<std::string>(std::real(-gu_dt/gu_dy/2.0))},
+            {"\"_gu_dt_dz_2_\"", boost::lexical_cast<std::string>(std::real(gu_dt/gu_dz/2.0))},
+            {"\"_gu_m_dt_dz_2_\"", boost::lexical_cast<std::string>(std::real(-gu_dt/gu_dz/2.0))},
+            {"\"_gu_indxSave_\"", boost::lexical_cast<std::string>(gu_indxSave)},
+            {"\"_gu_indxSave_p1_\"", boost::lexical_cast<std::string>(gu_indxSave + 1)},
+            {"\"_gu_save_rate_\"", boost::lexical_cast<std::string>(gu_save_rate)},
             {"\"_nt_coarse_IP0_\"", boost::lexical_cast<std::string>(nt_ip0)},
             {"\"_nt_coarse_IP1_\"", boost::lexical_cast<std::string>(nt_ip1)},
             {"\"_mod_phase_\"", boost::lexical_cast<std::string>(M_PI/2.0)}
@@ -219,6 +279,12 @@ void test_run_fdtd_3d_nonuniform_gridcollection_from_json() {
     UtilityFunctions::WriteParamToFile<FPNumber>(paramFileOut, gl_y1, "gl_y1");
     UtilityFunctions::WriteParamToFile<FPNumber>(paramFileOut, gl_z0, "gl_z0");
     UtilityFunctions::WriteParamToFile<FPNumber>(paramFileOut, gl_z1, "gl_z1");
+    UtilityFunctions::WriteParamToFile<FPNumber>(paramFileOut, gu_x0, "gu_x0");
+    UtilityFunctions::WriteParamToFile<FPNumber>(paramFileOut, gu_x1, "gu_x1");
+    UtilityFunctions::WriteParamToFile<FPNumber>(paramFileOut, gu_y0, "gu_y0");
+    UtilityFunctions::WriteParamToFile<FPNumber>(paramFileOut, gu_y1, "gu_y1");
+    UtilityFunctions::WriteParamToFile<FPNumber>(paramFileOut, gu_z0, "gu_z0");
+    UtilityFunctions::WriteParamToFile<FPNumber>(paramFileOut, gu_z1, "gu_z1");
 
 }
 
